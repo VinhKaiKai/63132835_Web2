503604ad2f50008d68655721e465d6451fd8a985
Vinh KaiKai <tramquangvinh12@gmail.com>
2024-03-26 16:24:05 +0700

    lần 6

--- TramQuangVinh_QLSV/src/main/java/thiGK/ntu63132835/Controller/SinhVienController.java
+++ TramQuangVinh_QLSV/src/main/java/thiGK/ntu63132835/Controller/SinhVienController.java
@@ -14,8 +14,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.learncode.models.SinhVien;
 import com.learncode.services.SinhvienService;
+
+import thiGK.ntu63132835.models.SinhVien;
+import thiGK.ntu63132835.services.SinhVienService;
+
 
 @Controller
 public class SinhVienController {
--- TramQuangVinh_QLSV/src/main/java/thiGK/ntu63132835/services/SinhVienService.java
+++ TramQuangVinh_QLSV/src/main/java/thiGK/ntu63132835/services/SinhVienService.java
@@ -3,8 +3,8 @@ package thiGK.ntu63132835.services;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.learncode.models.SinhVien;
 
+import thiGK.ntu63132835.models.SinhVien;
 public interface SinhVienService {
 	public Page<SinhVien> findPaginated(Pageable pageable);
 }
