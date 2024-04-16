037ec014aa12b5ad94de6c357cbd4096d628a661
Vinh KaiKai <tramquangvinh12@gmail.com>
2024-04-16 15:27:19 +0700

    DuAn

---
+++ DuAn-Web2/src/main/java/vinhhoccode/spring/DuAnWeb2Application.java
@@ -0,0 +1,13 @@
+package vinhhoccode.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuAnWeb2Application.class, args);
+	}
+
+}
---
+++ DuAn-Web2/src/test/java/vinhhoccode/spring/DuAnWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package vinhhoccode.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAnWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
