7e38379da064f32518b2546fc3f713440789c937
Vinh KaiKai <tramquangvinh12@gmail.com>
2024-05-20 23:03:00 +0700

    Làm thêm chức năng gửi mail

---
+++ LayoutProject/src/main/java/learncode/spring/LayoutProjectApplication.java
@@ -0,0 +1,13 @@
+package learncode.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayoutProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayoutProjectApplication.class, args);
+	}
+
+}
